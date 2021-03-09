1dfdfcb3df0a07d5ad3eb8faf4a8134ef7af2fda
tsavo <tsavo@pieces.app>
2021-03-09 07:18:07 -0500

    Initial Typescript Compiler

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,21 @@
 
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
 
-import { babel } from '@rollup/plugin-babel';
-import { BabelLegacyConfig } from "./src/babel/legacy/config"
-import { BabelTC39Config } from "./src/babel/tc39-v2/config"
+
+// import { babel } from '@rollup/plugin-babel';
+// import { BabelLegacyConfig } from "./src/babel/legacy/config"
+// import { BabelTC39Config } from "./src/babel/tc39-v2/config"
 
 const isBabel = Boolean(parseInt(process.env.BABEL, 10));
-const isTypescript = !babel;
+const isTypescript = !isBabel;
 const isLegacy = process.env.DECORATORS === 'LEGACY' || isTypescript;
 
+console.log(isTypescript)
 export default {
 	// Pardon this chaos - T
-	input: './src' + (isBabel ? ('/babel' + (isLegacy ? '/legacy' : '/tc39-v2')) : '/typescript') + '/examples.js',
+	input: ('./src' + (isBabel ? ('/babel' + (isLegacy ? '/legacy' : '/tc39-v2')) : '/typescript') + ('/examples' + (isBabel ? '.js' : '.ts'))),
 	output: {
 		file: 'public/index.js',
 		sourcemap: true,
@@ -20,7 +23,8 @@ export default {
 	},
 	plugins: [
 		resolve(),
-		...(isBabel && [babel(isLegacy ? BabelLegacyConfig : BabelTC39Config)]),
-		commonjs(),
+		// ...(isBabel && [babel(isLegacy ? BabelLegacyConfig : BabelTC39Config)]),
+		...(isTypescript && [typescript({ useTsconfigDeclarationDir: true })]),
+		commonjs(...(isTypescript && { extensions: ['.js', '.ts'] })),
 	]
 };
