8c0dc759a49a278de9768442174d0f69049200e1
tsavo <tsavo@pieces.app>
2021-03-09 07:27:56 -0500

    All Three Builds for Decorator Specs Completed

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,14 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
-
-
-// import { babel } from '@rollup/plugin-babel';
-// import { BabelLegacyConfig } from "./src/babel/legacy/config"
-// import { BabelTC39Config } from "./src/babel/tc39-v2/config"
+import { BabelLegacyConfig } from "./src/babel/legacy/config"
+import { BabelTC39Config } from "./src/babel/tc39-v2/config"
+import { babel } from '@rollup/plugin-babel';
 
 const isBabel = Boolean(parseInt(process.env.BABEL, 10));
 const isTypescript = !isBabel;
 const isLegacy = process.env.DECORATORS === 'LEGACY' || isTypescript;
 
-console.log(isTypescript)
 export default {
 	// Pardon this chaos - T
 	input: ('./src' + (isBabel ? ('/babel' + (isLegacy ? '/legacy' : '/tc39-v2')) : '/typescript') + ('/examples' + (isBabel ? '.js' : '.ts'))),
@@ -23,8 +20,8 @@ export default {
 	},
 	plugins: [
 		resolve(),
-		// ...(isBabel && [babel(isLegacy ? BabelLegacyConfig : BabelTC39Config)]),
-		...(isTypescript && [typescript({ useTsconfigDeclarationDir: true })]),
-		commonjs(...(isTypescript && { extensions: ['.js', '.ts'] })),
+		...(isBabel ? [babel(isLegacy ? BabelLegacyConfig : BabelTC39Config)] : []),
+		...(isTypescript ? [typescript({ tsconfig: './src/typescript/tsconfig.json' })] : []),
+		commonjs({...(isTypescript && { extensions: ['.js', '.ts'] })}),
 	]
 };
